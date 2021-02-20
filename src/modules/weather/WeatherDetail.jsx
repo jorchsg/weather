ed99b161da148768f2c7bec05a28d56c77c259c0
Jorge Sandoval <sandoval.jorge.407@gmail.com>
2021-02-19 22:12:17 -0800

    main structure and necesary dependencies installed

---
+++ src/modules/weather/WeatherDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WeatherDetail = () => {
+
+
+
+    return (
+        <div>
+            <h1>Weather Detail</h1>
+        </div>
+    )
+}
+
+export default WeatherDetail;
---
+++ src/modules/weather/WeatherList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const WeatherList = () => {
+
+
+
+    return (
+
+        <div>
+            <h1>Weather List</h1>
+        </div>
+    )
+}
+
+export default WeatherList;
