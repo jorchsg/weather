d896b0639838e0c6a863813d9499d3b18c03ddc6
Jorge Sandoval <sandoval.jorge.407@gmail.com>
2021-02-20 15:01:36 -0800

    request weather API

--- src/modules/weather/Weather.jsx
+++ src/modules/weather/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from '../../library/common/components/header/Header';
 import Menu from '../../library/common/components/menu/Menu';
 import WeatherList from './WeatherList';
@@ -7,12 +7,50 @@ import WeatherDetail from './WeatherDetail';
 
 const Weather = () => {
 
+    // State 
+    const [cities, setCities] = useState({});
+
+    useEffect(() => {
+
+        const requestAPI = async () => {
+
+            if (cities) {
+                //Bounding Box Variables
+                const longLeft = -118;
+                const latBottom = 32;
+                const longRight = -116;
+                const latTop = 34;
+                const zoom = 9;
+
+                // Variables requiered to request API
+                const bbox = `${longLeft},${latBottom},${longRight},${latTop},${zoom}`;;
+                const apiId = '60bd76654b6269919baec6ed35543967';
+                const apiURL = 'https://api.openweathermap.org/data/2.5';
+
+                const url = `${apiURL}/box/city?bbox=${bbox}&appid=${apiId}`;
+
+                const response = await fetch(url);
+                const result = await response.json();
+
+                setCities(result);
+            }
+
+        }
+
+        requestAPI();
+
+    }, [])
+
+    console.log(cities);
+    console.log(cities.list);
+
+
     return (
         <Fragment>
             <Menu />
             <div className="container">
                 <Header
-                    title="Check The Current Weather"
+                    title="Weather Near To Your Location"
                 />
                 <div className="row">
                     <WeatherList
